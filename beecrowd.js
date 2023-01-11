8b90beb0a4f3a8f14156b734bc7c31ede5dac463
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-01-11 08:56:05 +0200

    added a beecrowd experiment field

--- beecrowd.js
+++ beecrowd.js
@@ -1,1 +1,9 @@
-console.log(document.getElementById("input"));
+// Това не го копираме
+let input = document.getElementById("input").value;
+
+// Копираме от тук надолу
+let lines = input.split("\n");
+let a = Number(lines[0]);
+let b = Number(lines[1]);
+let result = a + b;
+console.log("X = " + result);
