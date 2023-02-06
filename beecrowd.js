f0ccef227c6a09c6961dd47d9349e4c8008fb430
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-02-06 09:37:39 +0200

    add game canvas and calculator

--- beecrowd.js
+++
@@ -1,9 +0,0 @@
-// Това не го копираме
-let input = document.getElementById("input").value;
-
-// Копираме от тук надолу
-let lines = input.split("\n");
-let a = Number(lines[0]);
-let b = Number(lines[1]);
-let result = a + b;
-console.log("X = " + result);
